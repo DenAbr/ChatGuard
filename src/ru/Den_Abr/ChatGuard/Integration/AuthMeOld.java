d90c4a744e6af280b4038d97fa2453876a606fd3
Den_Abr <ia12345@mail.ru>
2015-08-19 22:11:02 +0600

    Escape ClassNotFoundException

---
+++ src/ru/Den_Abr/ChatGuard/Integration/AuthMeOld.java
@@ -0,0 +1,40 @@
+package ru.Den_Abr.ChatGuard.Integration;
+
+import org.bukkit.Bukkit;
+import org.bukkit.entity.Player;
+import org.bukkit.plugin.Plugin;
+import org.bukkit.plugin.java.JavaPlugin;
+
+import uk.org.whoami.authme.cache.auth.PlayerCache;
+
+public class AuthMeOld extends AbstractPlugin {
+	private JavaPlugin plugin;
+
+	@Override
+	public boolean skipPlayer(Player p) {
+		return !PlayerCache.getInstance().isAuthenticated(p.getName());
+	}
+
+	@Override
+	public JavaPlugin getPlugin() {
+		return plugin;
+	}
+
+	@Override
+	public boolean load() {
+		Plugin pl = Bukkit.getPluginManager().getPlugin("AuthMe");
+		if (null == pl)
+			return false;
+		if (pl.getDescription().getAuthors().contains("whoami")) {
+			plugin = (JavaPlugin) pl;
+		}
+		return plugin != null && pl.isEnabled();
+	}
+
+	@Override
+	public void register() {
+		if (load()) {
+			getIntegratedPlugins().add(this);
+		}
+	}
+}
