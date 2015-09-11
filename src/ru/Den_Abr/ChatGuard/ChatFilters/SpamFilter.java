8f91b4257f8e6a391d4921cc820baea16b6d2780
Денис <ia12345@mail.ru>
2015-09-12 01:45:24 +0600

    Improve MaxNums counter

--- src/ru/Den_Abr/ChatGuard/ChatFilters/SpamFilter.java
+++ src/ru/Den_Abr/ChatGuard/ChatFilters/SpamFilter.java
@@ -46,13 +46,9 @@ public class SpamFilter extends AbstractFilter {
 			v = Violation.SPAM;
 		}
 		if (maxNums > 0) {
-			int charCount = 0;
-			for (char c : message.replace(" ", "").toCharArray()) {
-				if (Character.isDigit(c)) {
-					charCount++;
-				}
-			}
-			if (charCount > maxNums)
+			int numCount = message.replaceAll("[^0-9]", "").length();
+			ChatGuardPlugin.debug(1, "Numerics count: " + numCount);
+			if (numCount > maxNums)
 				v = Violation.SPAM;
 		}
 		if (v != null && informAdmins) {
